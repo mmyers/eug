214cc90ab128f96c7c67452418f5ffa716f7e99e
cicero66 <>
2007-09-24 21:16:41 +0000

    Event

---
+++ EugFile_specific/src/eug/specific/eu3/EU3Event.java
@@ -0,0 +1,36 @@
+/*
+ * EU3Event.java
+ *
+ * Created on September 12, 2007, 2:31 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package eug.specific.eu3;
+
+import eug.shared.GenericObject;
+
+
+/**
+ *
+ * @author jeff
+ */
+public class EU3Event  extends EU3SpecificObject {
+    
+    /** Creates a new instance of EU3Event */
+    public EU3Event(GenericObject g, EU3DataSource ds) {
+        super(g, ds);
+    }
+    
+    public int getID() {
+        return 0;
+    }
+    
+    public String getIDAsString() {
+        return "";
+    }
+    
+    
+    
+}
