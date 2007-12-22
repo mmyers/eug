b5c08bd96f511b77264c23b64d4e28c99a58b4cc
mmyers <m_myers@users.sourceforge.net>
2007-12-22 04:03:19 +0000

    Initial version

---
+++ EU3_Scenario_Editor/src/editor/mapmode/NativeGroupMode.java
@@ -0,0 +1,44 @@
+/*
+ * NativeGroupMode.java
+ *
+ * Created on December 19, 2007, 5:38 PM
+ */
+
+package editor.mapmode;
+
+import editor.Main;
+import editor.MapPanel;
+import editor.ProvinceData.Province;
+import java.util.List;
+
+/**
+ *
+ * @author Michael Myers
+ * @since 0.6pre1
+ */
+public class NativeGroupMode extends GroupMode {
+    
+    public NativeGroupMode(String nativeGroup) {
+        super(Main.map.getNatives(nativeGroup));
+    }
+    
+    public NativeGroupMode(List<String> nativeGroup) {
+        super(nativeGroup);
+    }
+    
+    public NativeGroupMode(MapPanel panel, String nativeGroup) {
+        super(panel, Main.map.getNatives(nativeGroup));
+    }
+    
+    public NativeGroupMode(MapPanel panel, List<String> nativeGroup) {
+        super(panel, nativeGroup);
+    }
+    
+    @Override
+    public String getTooltipExtraText(final Province current) {
+        if (current.getId() == 0 || current.getId() >= SEA_STARTS)
+            return "";
+        
+        return "Natives type: " + Main.map.getNativeTypeOfProv(current.getId());
+    }
+}
