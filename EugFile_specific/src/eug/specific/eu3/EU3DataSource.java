9c2eac1b6129054fbc72bb70c4b034db9ac9c973
mmyers <m_myers@users.sourceforge.net>
2008-01-05 01:21:22 +0000

    Fix Javadoc.

--- EugFile_specific/src/eug/specific/eu3/EU3DataSource.java
+++ EugFile_specific/src/eug/specific/eu3/EU3DataSource.java
@@ -20,42 +20,82 @@ public interface EU3DataSource {
     
     /**
      * Returns the country data for the country with the given tag.
+     * <p/>
+     * In some implementations, this may be equivalent to
+     * {@link #getCountryHistory}.
+     * @param tag the country tag to fetch data for.
+     * @return the country data.
      */
     public GenericObject getCountry(String tag);
     
     /**
      * Returns the country history for the country with the given tag.
+     * <p/>
+     * In some implementations, this may be equivalent to
+     * {@link #getCountry}.
+     * @param tag the country tag to fetch data for.
+     * @return the country history.
      */
     public GenericObject getCountryHistory(String tag);
     
     /**
      * Returns the province data for the province with the given ID.
+     * <p/>
+     * In some implementations, this may be equivalent to
+     * {@link #getProvinceHistory}.
+     * @param id the ID of the province to fetch data for.
+     * @return the province data.
      */
     public GenericObject getProvince(int id);
     
     /**
      * Returns the province history for the province with the given ID.
+     * <p/>
+     * In some implementations, this may be equivalent to
+     * {@link #getProvince}.
+     * @param id the ID of the province to fetch data for.
+     * @return the province data.
      */
     public GenericObject getProvinceHistory(int id);
     
     
     /**
      * Returns the country data for the country with the given tag.
+     * <p/>
+     * This may or may not be equivalent to calling
+     * <code>getCountry(tag).toString()</code>.
+     * @param tag the country tag to fetch data for.
+     * @return the country data in the form of a string.
      */
     public String getCountryAsStr(String tag);
     
     /**
      * Returns the country history for the country with the given tag.
+     * <p/>
+     * This may or may not be equivalent to calling
+     * <code>getCountryHistory(tag).toString()</code>.
+     * @param tag the country tag to fetch data for.
+     * @return the country data in the form of a string.
      */
     public String getCountryHistoryAsStr(String tag);
     
     /**
      * Returns the province data for the province with the given ID.
+     * <p/>
+     * This may or may not be equivalent to calling
+     * <code>getProvince(tag).toString()</code>.
+     * @param id the ID of the province to fetch data for.
+     * @return the province data in the form of a string.
      */
     public String getProvinceAsStr(int id);
     
     /**
      * Returns the province history for the province with the given ID.
+     * <p/>
+     * This may or may not be equivalent to calling
+     * <code>getProvinceHistory(tag).toString()</code>.
+     * @param id the ID of the province to fetch data for.
+     * @return the province data in the form of a string.
      */
     public String getProvinceHistoryAsStr(int id);
     
@@ -63,35 +103,47 @@ public interface EU3DataSource {
     /**
      * Forces a reload of the data for the country with the given tag, if the
      * implementation uses a cache.
+     * @param tag the tag of the country to reload data for.
      */
     public void reloadCountry(String tag);
     
     /**
      * Forces a reload of the history for the country with the given tag, if the
      * implementation uses a cache.
+     * @param tag the tag of the country to reload data for.
      */
     public void reloadCountryHistory(String tag);
     
     /**
      * Forces a reload of the data for the province with the given ID, if the
      * implementation uses a cache.
+     * @param id the ID of the province to reload data for.
      */
     public void reloadProvince(int id);
     
     /**
      * Forces a reload of the history for the province with the given ID, if the
      * implementation uses a cache.
+     * @param id the ID of the province to reload data for.
      */
     public void reloadProvinceHistory(int id);
     
     
     /**
-     * Saves the data for the given country.
+     * Saves the data for the given country. Depending on the implementation, a
+     * call to {@link #saveChanges()} may still be necessary.
+     * @param tag the tag of the country.
+     * @param cname the name of the country (mainly used for filename purposes).
+     * @param data the country data to save.
      */
     public void saveCountry(String tag, String cname, final String data);
     
     /**
-     * Saves the data for the given province.
+     * Saves the data for the given province. Depending on the implementation, a
+     * call to {@link #saveChanges()} may still be necessary.
+     * @param id the ID of the province.
+     * @param pname the name of the province (mainly used for filename purposes).
+     * @param data the province data to save.
      */
     public void saveProvince(int id, String pname, final String data);
     
@@ -103,6 +155,8 @@ public interface EU3DataSource {
     /**
      * Returns true if there are changes that have not yet been permanently
      * saved.
+     * @return <code>true</code> if there are unsaved changes,
+     * <code>false</code> otherwise.
      */
     public boolean hasUnsavedChanges();
     
@@ -110,9 +164,13 @@ public interface EU3DataSource {
     /**
      * Preloads all province data for provinces with IDs up to
      * <code>last</code>, usually into a cache.
-     * Some implementations may go further, if preloading is inexpensive.
+     * <p/>
+     * Some implementations may go further, if preloading is inexpensive (i.e.,
+     * does not involve file I/O).
+     * @param last the ID of the last province to preload.
      */
     public void preloadProvinces(int last);
+    
     /**
      * Preloads all country data (usually into a cache).
      */
