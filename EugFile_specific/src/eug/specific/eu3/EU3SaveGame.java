f19f15ca0944bcc54ccb34c0ba06c59f36f452c5
mmyers <m_myers@users.sourceforge.net>
2011-03-16 03:28:58 +0000

    Moved common behavior to the new Clausewitz classes

--- EugFile_specific/src/eug/specific/eu3/EU3SaveGame.java
+++ EugFile_specific/src/eug/specific/eu3/EU3SaveGame.java
@@ -10,8 +10,8 @@ import eug.parser.EUGFileIO;
 import eug.shared.FilenameResolver;
 import eug.shared.GenericObject;
 import eug.shared.ObjectVariable;
-import eug.shared.Scenario;
 import eug.shared.Style;
+import eug.specific.clausewitz.ClausewitzSaveGame;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -22,28 +22,15 @@ import java.util.Map;
  * @author Michael Myers
  * @since EUGFile 1.03.00
  */
-public class EU3SaveGame extends Scenario implements EU3DataSource {
-    
-    private final String savePath;
+public class EU3SaveGame extends ClausewitzSaveGame {
     
     private final FilenameResolver resolver;
-    
-    private Map<String, GenericObject> countryMap;
-//    private List<GenericObject> provinces;
-    
-    private int lastProvId;
-    
-    private GenericObject diplomacy;
-    
-    private boolean hasUnsavedChanges = false;
     
     /**
      * Creates a new instance of EU3SaveGame
      */
     public EU3SaveGame(GenericObject root, String savePath, String mainPath, String modName) {
-        this.root = root;
-        this.scenarioName = "";
-        this.savePath = savePath;
+        super(root, savePath, mainPath, modName);
         
         resolver = new FilenameResolver(mainPath, modName);
         
@@ -53,9 +40,7 @@ public class EU3SaveGame extends Scenario implements EU3DataSource {
     }
     
     public EU3SaveGame(GenericObject root, String savePath, FilenameResolver resolver) {
-        this.root = root;
-        this.scenarioName = "";
-        this.savePath = savePath;
+        super(root, savePath, null, null);
         this.resolver = resolver;
     }
     
@@ -135,15 +120,6 @@ public class EU3SaveGame extends Scenario implements EU3DataSource {
     }
     
     
-    public List<String> getCountryTags() {
-        return new ArrayList<String>(getCountryMap().keySet());
-    }
-    
-    public List<GenericObject> getCountries() {
-        return new ArrayList<GenericObject>(getCountryMap().values());
-    }
-    
-    
     public EU3Country getEU3Country(final String tag) {
         return new EU3Country(getCountryMap().get(tag.substring(0,3).toUpperCase()), this);
     }
@@ -152,181 +128,13 @@ public class EU3SaveGame extends Scenario implements EU3DataSource {
         return new EU3Province(getProvinces().get(id + 1), this);
     }
     
-    public GenericObject getCountry(String tag) {
-        return getCountryMap().get(tag.substring(0,3).toUpperCase());
-    }
     
-    public GenericObject getCountryHistory(String tag) {
-        return getCountryMap().get(tag.substring(0,3).toUpperCase()).getChild("history");
-    }
-    
-    public GenericObject getProvince(int id) {
-        if (id > lastProvId)
-            return null;
-        return getProvinces().get(id-1);
-    }
-    
-    public GenericObject getProvinceHistory(int id) {
-        return getProvinces().get(id-1).getChild("history");
-    }
-    
-    public List<GenericObject> getWars() {
-        final List<GenericObject> ret = root.getChildren("active_war");
-        ret.addAll(root.getChildren("previous_war"));
-        return ret;
-    }
-    
-    public List<GenericObject> getWars(String date) {
-        final List<GenericObject> ret = new ArrayList<GenericObject>();
-        
-        // Only check wars if the date is in the past
-        if (EU3History.DATE_COMPARATOR.isBefore(date, getDate())) {
-            for (GenericObject war : root.getChildren("active_war")) {
-                final List<String> participants =
-                        EU3History.getHistStrings(war, date, "add_attacker", "rem_attacker");
-                
-                if (!participants.isEmpty())
-                    ret.add(war);
-            }
-        
-            for (GenericObject war : root.getChildren("previous_war")) {
-                final List<String> participants =
-                        EU3History.getHistStrings(war, date, "add_attacker", "rem_attacker");
-                
-                if (!participants.isEmpty())
-                    ret.add(war);
-            }
-        }
-        
-        return ret;
-    }
-
-    public void removeWar(String name) {
-        GenericObject toRemove = null;
-        for (GenericObject war : getWars()) {
-            if (war.getString("name").equals(name)) {
-                toRemove = war;
-                break;
-            }
-        }
-        if (toRemove != null) {
-            toRemove.getParent().removeChild(toRemove);
-            hasUnsavedChanges = true;
-        }
-    }
-    
-    
-    public String getCountryAsStr(String tag) {
-        return getCountryMap().get(tag.substring(0,3).toUpperCase()).toString(Style.EU3_SAVE_GAME);
-    }
-    
-    public String getCountryHistoryAsStr(String tag) {
-        return getCountryMap().get(tag.substring(0,3).toUpperCase()).getChild("history").toString(Style.EU3_SAVE_GAME);
-    }
-    
-    public String getProvinceAsStr(int id) {
-        return getProvinces().get(id-1).toString(Style.EU3_SAVE_GAME);
-    }
-    
-    public String getProvinceHistoryAsStr(int id) {
-        return getProvinces().get(id-1).getChild("history").toString(Style.EU3_SAVE_GAME);
-    }
-    
-    public void saveCountry(String tag, String cname, final String data) {
-        final GenericObject country = getCountryMap().get(tag);
-        country.clear();
-        final GenericObject newCountry = EUGFileIO.loadFromString(data);
-        country.addAllChildren(newCountry.getChild(tag));
-        hasUnsavedChanges = true;
-    }
-    
-    public void saveProvince(int id, String pname, final String data) {
-        final GenericObject province = getProvinces().get(id-1);
-        province.clear();
-        final GenericObject newProvince = EUGFileIO.loadFromString(data);
-        province.addAllChildren(newProvince.getChild(Integer.toString(id)));
-        hasUnsavedChanges = true;
-    }
-
-    public void saveWar(String name, String data) {
-        final GenericObject newWar = EUGFileIO.loadFromString(data);
-        for (GenericObject war : getWars()) {
-            if (war.getString("name").equals(name)) {
-                war.clear();
-                war.addAllChildren(newWar.getChild(0));
-                hasUnsavedChanges = true;
-                return;
-            }
-        }
-    }
-    
+    @Override
     public void saveChanges() {
         if (hasUnsavedChanges) {
             EUGFileIO.save(root, savePath, EUGFileIO.NO_COMMENT, true, Style.EU3_SAVE_GAME);
             hasUnsavedChanges = false;
         }
-    }
-    
-    public boolean hasUnsavedChanges() {
-        return hasUnsavedChanges;
-    }
-    
-    
-    public void reloadCountry(String tag) {
-        // What to do here?
-        initCountries();
-    }
-    
-    public void reloadCountryHistory(String tag) {
-        // What to do here?
-        initCountries();
-    }
-    
-    public void reloadProvince(int id) {
-        // What to do here?
-        initProvs();
-    }
-    
-    public void reloadProvinceHistory(int id) {
-        // What to do here?
-        initProvs();
-    }
-    
-    public void preloadProvinces(int last) {
-        // ignore parameter, since it's so quick anyway
-        preloadProvinces();
-    }
-    
-    public void preloadProvinces() {
-        if (provinces == null)
-            initProvs();
-    }
-    
-    public void preloadCountries() {
-        if (countries == null)
-            initCountries();
-    }
-    
-    
-    @Override
-    public int numCountries() {
-        return getCountryMap().size();
-    }
-    
-    
-    public String getDate() {
-        return root.getString("date");
-    }
-    
-    public int getLastProvID() {
-        return lastProvId;
-    }
-    
-    // Lazy accessor
-    private GenericObject getDiplomacy() {
-        if (diplomacy == null)
-            diplomacy = root.getChild("diplomacy");
-        return diplomacy;
     }
     
     public List<GenericObject> getAlliances() {
