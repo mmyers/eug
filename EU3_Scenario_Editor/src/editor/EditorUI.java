a5e93f2225f8b0a504473ac654d81f268b40dc66
mmyers <m_myers@users.sourceforge.net>
2011-03-22 13:51:59 +0000

    Updated version

--- EU3_Scenario_Editor/src/editor/EditorUI.java
+++ EU3_Scenario_Editor/src/editor/EditorUI.java
@@ -49,7 +49,7 @@ public final class EditorUI extends javax.swing.JFrame {
 //    /** The province that is selected in the combo box at the left. */
 //    private transient ProvinceData.Province selectedProvince;
     
-    private static final String VERSION = "0.7";
+    private static final String VERSION = "0.7.1";
     
     private JPopupMenu bookmarkMenu = new JPopupMenu("Bookmarks");
 
