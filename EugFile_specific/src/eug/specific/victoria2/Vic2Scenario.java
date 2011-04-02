7a3bb7a1682803859c7addef0abb0eb32a06b6b6
mmyers <m_myers@users.sourceforge.net>
2011-04-02 06:20:32 +0000

    Overrode saveProvince to work properly with subfolders

--- EugFile_specific/src/eug/specific/victoria2/Vic2Scenario.java
+++ EugFile_specific/src/eug/specific/victoria2/Vic2Scenario.java
@@ -3,6 +3,7 @@ package eug.specific.victoria2;
 
 import eug.shared.FilenameResolver;
 import eug.specific.clausewitz.ClausewitzScenario;
+import java.io.File;
 
 /**
  *
@@ -21,6 +22,32 @@ public class Vic2Scenario extends ClausewitzScenario {
     @Override
     protected String resolveProvinceHistoryFile(int id) {
         return resolver.getVic2ProvinceHistoryFile(id);
+    }
+
+    @Override
+    public void saveProvince(int id, String pname, String data) {
+        String filename = resolveProvinceHistoryFile(id);
+        if (filename == null) {
+            // how do we decide which subfolder to put the new file in?
+            // just put it in the main provinces directory for now (still works)
+            filename = resolver.resolveDirectory("history") + "provinces/" +
+                    id + " - " + pname + ".txt";
+        } else {
+            String subfolder = new File(filename).getParentFile().getName();
+            if (!subfolder.equalsIgnoreCase("provinces"))
+                subfolder = "provinces/" + subfolder;
+            filename = filename.substring(Math.max(filename.lastIndexOf('/'), filename.lastIndexOf('\\')));
+            filename = resolver.resolveDirectory("history") + subfolder + filename;
+        }
+
+        final File file = new File(filename);
+        if (file.exists()) {
+            final String backupFilename = getBackupFilename(filename);
+            if (!file.renameTo(new File(backupFilename)))
+                System.err.println("Backup of " + file.getName() + " failed");
+        }
+
+        saveFile(filename, data);
     }
 
 }
