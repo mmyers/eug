6eaa8370686886f42e82877d6fb1cff8d79251a2
mmyers <mmyers@users.noreply.github.com>
2024-06-20 11:50:20 -0500

    Clean up ReligionMode and don't cache there
    Since MapPanel now only paints when necessary, ReligionMode should always paint when asked to

--- EU3_Scenario_Editor/src/editor/mapmode/ReligionMode.java
+++ EU3_Scenario_Editor/src/editor/mapmode/ReligionMode.java
@@ -22,9 +22,7 @@ import java.util.List;
  */
 public final class ReligionMode extends ProvincePaintingMode {
     
-    private String lastDate = null;
-    private final java.util.Map<String, String> ctryReligions =
-            new HashMap<String, String>();
+    private final java.util.Map<String, String> ctryReligions = new HashMap<>();
     
     /**
      * Creates a new instance of ReligionMode.
@@ -39,8 +37,6 @@ public final class ReligionMode extends ProvincePaintingMode {
     @Override
     protected void paintingStarted(final Graphics2D g) {
         final MapPanelDataModel model = mapPanel.getModel();
-        if (lastDate != null && lastDate.equals(model.getDate()))
-            return;
 
         ctryReligions.clear();
 
@@ -48,36 +44,34 @@ public final class ReligionMode extends ProvincePaintingMode {
         for (String tag : tags) {
             ctryReligions.put(tag.toUpperCase(), model.getHistString(tag, "religion"));
         }
-
-        lastDate = model.getDate();
     }
     
     protected void paintProvince(final Graphics2D g, int provId) {
+        if (getMap().isWasteland(provId)) {
+            mapPanel.paintProvince(g, provId, java.awt.Color.BLACK);
+            return;
+        }
+        
         final String religion = mapPanel.getModel().getHistString(provId, "religion");
         
-        if (getMap().isWasteland(provId))
-            mapPanel.paintProvince(g, provId, java.awt.Color.BLACK);
-        else if (religion == null) {
+        if (religion == null) {
             mapPanel.paintProvince(g, provId, Utilities.COLOR_NO_HIST);
         } else if (religion.length() == 0 || religion.equalsIgnoreCase("none")) {
             mapPanel.paintProvince(g, provId, Utilities.COLOR_NO_RELIGION);
         } else {
             final String owner = mapPanel.getModel().getOwner(provId).toUpperCase();
-            //final String ownerRel = (Utilities.isNotACountry(owner) ? null : mapPanel.getModel().getHistString(owner, "religion"));
             final String ownerRel = ctryReligions.get(owner);
 
             if (ownerRel == null || religion.equalsIgnoreCase(ownerRel)) {
                 mapPanel.paintProvince(g, provId, Utilities.getReligionColor(religion));
             } else {
                 final Paint p = Utilities.createPaint(Utilities.getReligionColor(religion), Utilities.getReligionColor(ownerRel));
-//                if (p != null)
-                    mapPanel.paintProvince(g, provId, p);
-//                else
-//                    System.err.println("Unknown problem in ReligionMode.java");
+                mapPanel.paintProvince(g, provId, p);
             }
         }
     }
     
+    @Override
     protected void paintSeaZone(final Graphics2D g, int id) {
         // do nothing
     }
@@ -97,7 +91,7 @@ public final class ReligionMode extends ProvincePaintingMode {
         String ctryRel = null;
         
         if (owner != null) {
-            if (Utilities.isNotACountry(owner) /*noCountryPattern.matcher(owner).matches()*/) {
+            if (Utilities.isNotACountry(owner)) {
                 // don't add anything
                 owner = "-";
             } else {
