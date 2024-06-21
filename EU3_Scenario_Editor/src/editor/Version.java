4a2c387c207d40865b849d26b942246dfd127ce7
mmyers <mmyers@users.noreply.github.com>
2024-06-21 18:23:19 -0500

    Bump to 1.0.1

--- EU3_Scenario_Editor/src/editor/Version.java
+++ EU3_Scenario_Editor/src/editor/Version.java
@@ -26,7 +26,7 @@ public class Version {
     
     private static final int MAJOR = 1;
     private static final int MINOR = 0;
-    private static final int REV = 0;
+    private static final int REV = 1;
     
     private static final String PROJECT_URL = "https://sourceforge.net/projects/eug";
     private static final String UPDATE_URL = "https://eug.sourceforge.net/version.xml";
